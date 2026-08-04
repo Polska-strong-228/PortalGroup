79ab226802d1d3b93614cf31060a505a478d0038
Polska-strong-228 <golubgeorgij0@gmail.com>
2026-08-04 21:59:16 +0300

    Update urls.py

--- apps/gallery/urls.py
+++ apps/gallery/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
+from . import views
 
 app_name = 'gallery'
 
 urlpatterns = [
+    path('', views.album_list, name='album_list'),
+    path('album/create/', views.album_create, name='album_create'),
+    path('album/<int:pk>/', views.album_detail, name='album_detail'),
+    path('album/<int:album_pk>/upload/', views.photo_upload, name='photo_upload'),
 ]
